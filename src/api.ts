8169521c24aa1fa12cd6e001f060e575d9bea415
CrACK <patrikfejfar2@gmail.com>
2024-03-11 22:30:58 +0100

    in process...

--- src/api.ts
+++ src/api.ts
@@ -1,18 +1,34 @@
-import express from 'express';
-import cors from 'cors';
-import router from './router';
-import passport from './plugins/passport';
-import bodyParser from 'body-parser';
+import express from 'express'
+import cors from 'cors'
+import router from './router'
+import passport from './plugins/passport'
+import bodyParser from 'body-parser'
+import session from 'express-session'
+import MongoStore from 'connect-mongo'
 
-export const app = express();
+export const app = express()
 
-app.set('trust proxy', true);
+app.set('trust proxy', true)
 
-app.use(cors({ origin: process.env.FRONTEND, credentials: true }));
+app.use(cors({ origin: process.env.FRONTEND, credentials: true }))
+
+
+const sessionStore = MongoStore.create({
+  mongoUrl: process.env.DB_URL,
+  dbName: process.env.DB_NAME,
+  collectionName: 'sessions'
+})
+
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: false,
+  store: sessionStore,
+}))
 
 app.use(require('express-session')({ secret: 'keyboard cat', resave: false, saveUninitialized: false }))
 app.use(passport.initialize())
-app.use(passport.session(undefined))
+app.use(passport.session())
 app.use(bodyParser.json())
 
-app.use('/api/v1', router);
+app.use('/api/v1', router)
