e823c693a93ec8e5e437205a6d048142e47b6c27
Patrik Fejfár <patrikfejfar2@gmail.com>
2024-03-17 15:04:15 +0100

    Debug

--- src/api.ts
+++ src/api.ts
@@ -19,7 +19,7 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
 app.use(helmet())
-app.use(cors({ origin: "*", credentials: true }))
+app.use(cors({ origin: process.env.FRONTEND, credentials: true }))
 
 
 const sessionStore = MongoStore.create({
