b962db2d3e95955cdc7dba3770a9d5c231a5f081
Patrik Fejfár <patrikfejfar2@gmail.com>
2024-03-22 17:05:55 +0100

    Debug

--- src/api.ts
+++ src/api.ts
@@ -1,48 +1,30 @@
 import express from 'express'
-import MongoStore from 'connect-mongo'
 import mongoose from 'mongoose'
 import lusca from 'lusca'
 import cors from 'cors'
-import session from 'express-session'
 import bodyParser from 'body-parser'
-import flash from 'express-flash'
-import passport from 'passport'
 import router from './middlewares/api.middleware'
 import auth from './middlewares/auth.middleware'
 
-export const app = express()
+export const app = express();
+app.use(cors({ origin: process.env.FRONTEND, credentials: true }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-mongoose.connect(process.env.DB_URL)
+mongoose
+  .connect(`${process.env.DB_URL}/${process.env.DB_NAME}?authSource=admin`)
   .then(() => {
-    console.log(`✅ Database is connected`)
+    console.log(`✅ Database is connected`);
   })
-  .catch(err => {
-    console.log(`⛔ MongoDB connection error. Please make sure MongoDB is running. ${err}`)
-    process.exit()
-  })
+  .catch((err) => {
+    console.log(
+      `⛔ MongoDB connection error. Please make sure MongoDB is running. ${err}`
+    );
+    process.exit();
+  });
 
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(cors({ origin: process.env.FRONTEND, credentials: true }))
-app.use(session({
-  name: "Spravime Sessions",
-  secret: process.env.SESSION_SECRET || "secret123",
-  resave: false,
-  saveUninitialized: false,
-  store: new MongoStore({
-    mongoUrl: process.env.DB_URL,
-    dbName: process.env.DB_NAME,
-    collectionName: 'sessions'
-  }),
-  cookie: {
-    secure: process.env.RAILWAY_ENVIRONMENT_NAME === "production"
-  }
-}))
-app.use(passport.initialize())
-app.use(passport.session())
-app.use(flash())
-app.use(lusca.xframe("SAMEORIGIN"))
-app.use(lusca.xssProtection(true))
+app.use(lusca.xframe('SAMEORIGIN'));
+app.use(lusca.xssProtection(true));
 
 const VERSION = '/api/v1'
 
